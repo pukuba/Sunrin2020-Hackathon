0910360bb8a321728e2bd8862e35ef6384ecea3d
pukuba <skatmddnjs0604@gmail.com>
2020-08-07 15:25:43 +0900

    init

---
+++ back-end/index.js
@@ -0,0 +1,60 @@
+const { ApolloServer,PubSub } = require('apollo-server-express')
+const express = require('express')
+const expressPlayground = require('graphql-playground-middleware-express').default
+const { readFileSync } = require('fs')
+const { MongoClient } = require('mongodb')
+
+const { createServer } = require('http')
+
+const depthLimit = require('graphql-depth-limit')
+const { createComplexityLimitRule } = require('graphql-validation-complexity')
+
+const path = require('path')
+require('dotenv').config()
+
+const resolvers = require('./resolvers')
+const typeDefs = readFileSync('./typeDefs.graphql', 'utf-8')
+
+const start  = async() => {
+    const app = express() 
+    const pubsub = new PubSub()
+    const client = await MongoClient.connect(
+        process.env.DB_HOST,
+        {
+            useUnifiedTopology: true,
+            useNewUrlParser : true,
+        }
+    ) 
+    const db = client.db()
+    
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        validationRules: [
+            depthLimit(10),
+            createComplexityLimitRule(10000, {
+                onCost: cost => console.log('query cost: ', cost)
+            })
+        ],
+        context: async({ req, connection }) => {
+            const token = req ? req.headers.authorization : connection.context.Authorization
+            return {db, token, pubsub}
+        }
+    })
+    server.applyMiddleware({ app })
+
+    app.get('/playground',expressPlayground({ endpoint: '/graphql'}))
+
+    const httpServer = createServer(app)
+    server.installSubscriptionHandlers(httpServer)
+    
+    httpServer.timeout = 10000
+
+    httpServer.listen({ port : 2222}, () =>{
+        console.log(`GQL Server running at http://localhost:2222${server.graphqlPath}`)
+        console.log(`Subscriptions ready at ws://localhost:2222${server.subscriptionsPath}`)
+        }
+    )
+}
+
+start()
